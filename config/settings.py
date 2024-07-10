c984519303ddfaff83f3cb6151e9f6d8348440dc
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-10 09:06:02 -0400

    Add All Changes

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = "/home/sina/01-Repo/onlineShope/static"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
