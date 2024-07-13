7dc19ccc25b0c558eecd3db3b32001acbb5d5d1c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-13 16:11:26 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -506,6 +506,14 @@ account
 
 
 
+# DELETE All from accounts/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view(), name='signup')
+]
+
+
 
 
 
