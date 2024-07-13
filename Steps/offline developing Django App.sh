b41f9e0d74ec70ed93ffc8f7573788d80158501c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-13 11:27:32 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -483,5 +483,6 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 SITE_ID = 1
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 
-# For allauth Package                          --------------->>>> SINA 
+# For allauth Package                          ==============>>>> SINA 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -167,5 +167,5 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 SITE_ID = 1
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
