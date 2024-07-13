1da48a45f0ddab298325135a071a2cf3eb71a5b9
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-13 05:25:31 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -289,6 +289,18 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup')
 ]
 
+# Add in accounts/views.py
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+
+
+
 
 
 
