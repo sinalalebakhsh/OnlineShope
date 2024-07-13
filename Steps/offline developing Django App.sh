972ea14b9da0bbae9c7686ce06da55d16b361394
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-13 02:55:48 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -224,6 +224,14 @@ INSTALLED_APPS = [ ...
 ]
 
 
+# Add in pages/views.py
+from django.views.generic import TemplateView
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+class AboutUsPageView(TemplateView):
+    template_name = 'pages/aboutus.html'
+    
+
 
 
 
