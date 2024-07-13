56dbdb8444d814d05e013b620abfb4bae46e3da7
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-13 03:16:12 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -258,6 +258,15 @@ urlpatterns = [
 # Create in pages
 urls.py
 
+# Add in pages/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.AboutUsPageView.as_view(), name='aboutus'),
+]
+
+
 
 
 
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from . import views
 
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.AboutUsPageView.as_view(), name='aboutus'),
+]
 
 
