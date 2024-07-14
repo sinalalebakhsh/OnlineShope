2f24f9e9baf4b7d4d20e417f3b1652856e13749f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-14 16:17:04 -0400

    Add All Changes

--- After_each_Lunch.sh
+++ After_each_Lunch.sh
@@ -1,11 +1,18 @@
 touch .env
 # Copy and Paste data to .env manualy
+nano .env
 
-
-# docker-compose up --build 
+docker-compose up --build 
 # Check and Ctrl + C
 
 # Than:
-# docker-compose up --build -d
+docker-compose up --build -d
+
+# For update pip
 docker-compose exec web  pip install --upgrade pip
+
+# For connect allauth package
 docker-compose exec web python manage.py migrate
+
+# For create super user
+docker-compose exec web python manage.py createsuperuser
