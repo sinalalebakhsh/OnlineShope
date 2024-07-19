f38c679f10f2fb729044742fa1294599ef366caa
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-19 06:27:16 -0400

    Add All Changes

--- Steps/offline developing Django App.sh
+++ Steps/offline developing Django App.sh
@@ -740,5 +740,12 @@ urlpatterns = [
     {% endfor %}
 
 # Add in products/admin.py
+from django.contrib import admin
+from .models import Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
 
 
