e739bc19780e0212bec620366bedf3115e4cdaab
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-21 11:01:07 -0400

    Add All Changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,24 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
-from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    list_display = ('email', 'username')
-
-
-
-
-
-
-
-
-
-
-
-
-
+    list_display = ("email", "username")
