99bc29c08506a1072231bba230d2db8260d87b2c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-21 10:12:10 -0400

    Add All Changes

--- products/admin.py
+++ products/admin.py
@@ -3,14 +3,26 @@ from django.contrib import admin
 from .models import Product
 from .models import Comment
 
+
+
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active']
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', ]
 
+    inlines = [
+        CommentsInline,
+    ]
+
 
 
 @admin.register(Comment)
-class ProductAdmin(admin.ModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = ['product', 'author', 'body', 'stars', 'active']
 
 
