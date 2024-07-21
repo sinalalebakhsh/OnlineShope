228faec7e4bf727ef76e631efc3e206b490e95ad
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-21 11:03:19 -0400

    Add All Changes

--- pages/views.py
+++ pages/views.py
@@ -2,21 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 class HomePageView(LoginRequiredMixin, TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class AboutPageView(LoginRequiredMixin, TemplateView):
-    template_name = 'pages/about.html'
-    
-
-
-
-
-
-
-
-
-
-
-
-
+    template_name = "pages/about.html"
