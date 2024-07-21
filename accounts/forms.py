54af9cfdca71ae69dacf50690b8f07cf6b1239a0
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-21 11:01:37 -0400

    Add All Changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,27 +2,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 
-
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username')
-
+        fields = ("email", "username")
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username')
-
-
-
-
-
-
-
-
-
-
-
-
+        fields = ("email", "username")
