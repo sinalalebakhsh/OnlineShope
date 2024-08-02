2eed1dfdcb2852347bdd894207dd7fd62373af9c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-08-01 22:38:07 -0400

    Add All Changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import CustomUserCreationForm
 
 
-class SignUpView(CreateView):
+class SignUpView(SuccessMessageMixin, CreateView):
     form_class = CustomUserCreationForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
+    success_message = "%(name)s was created successfully"
