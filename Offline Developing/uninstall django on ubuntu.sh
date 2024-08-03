7376ea5d3a40cc230539785a0775854e59fe6b91
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-08-03 16:10:18 -0400

    Add All Changes

--- Offline Developing/uninstall django on ubuntu.sh
+++ Offline Developing/uninstall django on ubuntu.sh
@@ -4,3 +4,7 @@ pip uninstall Django
 
 # Verify Uninstallation
 # If Django has been uninstalled successfully, you’ll receive an error stating that the command is not found.
+django-admin --version
+# Output: 
+    # Command 'django-admin' not found, but can be installed with:
+    # sudo apt install python3-django
