6686a1de675381eb7b0cb8da3495aa9810b3b55f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-08-17 17:24:13 -0400

    Add All Changes

--- cart/cart.py
+++ cart/cart.py
@@ -17,7 +17,11 @@ class Cart:
     def add(self, product, quantity=1):
         product_id = str(product.id)
 
-        
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+
+
+            
 
 
 
